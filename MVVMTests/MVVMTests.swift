50687a90ded73ebb51e2bf862470594c5808c72f
Nishant <nishant.shrestha@hotmail.com>
2017-10-29 11:15:37 +0545

    Added first test. I

--- MVVMTests/MVVMTests.swift
+++ MVVMTests/MVVMTests.swift
@@ -13,24 +13,20 @@ class MVVMTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testCarViewModelWithFerrariF12() {
+        let ferrariF12 = Car(make: "Ferrari", model: "F12", horsepower: 730, photoURL: "http://auto.ferrari.com/en_EN/wp-content/uploads/sites/5/2013/07/Ferrari-F12berlinetta.jpg")
+        let ferrariViewModel = CarViewModel(car: ferrariF12)
+        XCTAssertEqual(ferrariViewModel.modelText, "F12")
+        XCTAssertEqual(ferrariViewModel.makeText, "Ferrari")
+        XCTAssertEqual(ferrariViewModel.horsepowerText, "730 HP")
+        XCTAssertEqual(ferrariViewModel.photoURL, URL(string: ferrariF12.photoURL!))
+        XCTAssertEqual(ferrariViewModel.titleText, "Ferrari F12")
     }
     
 }
